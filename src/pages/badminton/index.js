269e36ec42452040384ec4e6325545f7addbb085
Oleen001 <kuptipong.park@gmail.com>
2024-01-11 21:17:23 +0700

    add table

--- src/pages/badminton/index.js
+++ src/pages/badminton/index.js
@@ -1,11 +1,62 @@
 import Button from '@atlaskit/button';
 import Calendar from '@atlaskit/calendar';
+import Textfield from '@atlaskit/textfield';
+import DynamicTable from '@atlaskit/dynamic-table';
 import "./style.css";
 
-const defaultPreviouslySelected = ['2020-12-06'];
-const defaultSelected = ['2020-12-08'];
+const days = ['Time', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
 
 function RegistBadminton() {
+
+    const head = {
+        cells: days.map((day) => ({
+            key: day,
+            content: day,
+        })),
+    };
+
+    const rows = [
+        {
+            key: `morning-row`,
+            cells: ['9:00', 'Math', 'History', 'Science', 'Computing', 'Math'].map(
+                (content, index) => ({
+                    key: index,
+                    content,
+                }),
+            ),
+        },
+        {
+            key: 'midday-row',
+            cells: [
+                {
+                    key: 0,
+                    content: '12:00',
+                },
+                {
+                    key: 1,
+                    content: 'LUNCH',
+                    colSpan: 5,
+                },
+            ],
+        },
+        {
+            key: 'afternoon-row',
+            cells: [
+                '13:00',
+                'Science',
+                'History',
+                'Psychology',
+                'Computing',
+                'Business',
+            ].map((content, index) => ({
+                key: index,
+                content,
+            })),
+        },
+    ];
+
+
     return (
         <div className='page-body'>
             <div className='card'>
@@ -16,7 +67,29 @@ function RegistBadminton() {
                     testId={'calendar'}
                     disabled='true'
                 />
-                <Button appearance="primary">เพิ่มชื่อเลยจ้า</Button>
+                <div className='form'>
+                    <div className='input-name'>
+                        <div className='input'>
+                            <Textfield
+                                appearance="standard"
+                                placeholder="ชื่อจ้า"
+                                style={{ height: "30px" ,borderRadius: "16px"}}
+                            />
+                        </div>
+                        <div className='btn'>
+                            <Button appearance="primary" shouldFitContainer >เพิ่มชื่อเลยจ้า</Button>
+                        </div>
+                    </div>
+                    <DynamicTable
+                        head={head}
+                        rows={rows}
+                        rowsPerPage={5}
+                        defaultPage={1}
+                        loadingSpinnerSize="large"
+                        isRankable
+                    />
+                </div>
+
             </div>
         </div>
     );
