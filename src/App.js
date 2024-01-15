59af793c7c2bae897373bc0e7ca6010a841833ad
Oleen001 <kuptipong.park@gmail.com>
2024-01-16 03:11:26 +0700

    img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import RegistBadminton from './pages/badminton';
 import * as React from 'react';
-import Lottie from "lottie-react";
-import Badminton from '../src/badminton.json'
 
 function App() {
   return (
     <div className='app-body'>
       <RegistBadminton></RegistBadminton>
-      <Lottie style={{ height: 300 }} animationData={Badminton} loop={true} />
     </div>
   );
 }
